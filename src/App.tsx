cb2bd3e217cecac9a2db1efbc536464351006bd3
Rian Septiawan <rianseptiawan@infitech.or.id>
2025-07-05 12:36:34 +0700

    rt

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ import Index from "./pages/Index";
 import Profil from "./pages/Profil";
 import LayananDigital from "./pages/LayananDigital";
 import UMKM from "./pages/UMKM";
+import UMKMDetail from "./pages/UMKMDetail";
 import Pariwisata from "./pages/Pariwisata";
+import PariwisataDetail from "./pages/PariwisataDetail";
 import InformasiPublik from "./pages/InformasiPublik";
 import BeritaDetail from "./pages/BeritaDetail";
 import Kontak from "./pages/Kontak";
@@ -27,7 +29,9 @@ const App = () => (
           <Route path="/profil" element={<Profil />} />
           <Route path="/layanan/*" element={<LayananDigital />} />
           <Route path="/umkm" element={<UMKM />} />
+          <Route path="/umkm/:id" element={<UMKMDetail />} />
           <Route path="/pariwisata" element={<Pariwisata />} />
+          <Route path="/pariwisata/:id" element={<PariwisataDetail />} />
           <Route path="/informasi" element={<InformasiPublik />} />
           <Route path="/informasi/berita/:id" element={<BeritaDetail />} />
           <Route path="/kontak" element={<Kontak />} />
